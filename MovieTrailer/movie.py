65b730f84b58d848126f83a3354a98c1a39bf2d6
applecool <kmitharsha@gmail.com>
2017-06-02 21:55:36 -0700

    added movie trailer

---
+++ MovieTrailer/movie.py
@@ -0,0 +1,19 @@
+import fresh_tomatoes
+import media
+
+wonder_woman = media.Movie("Wonder Woman",
+                           "A story of a warrior princess",
+                           "https://upload.wikimedia.org/wikipedia/en/e/ed/Wonder_Woman_%282017_film%29.jpg",
+                           "https://youtu.be/VSB4wGIdDwo")
+print(wonder_woman.storyline)
+
+justice_league = media.Movie("Justice League",
+                             "The story of legends",
+                             "https://upload.wikimedia.org/wikipedia/en/3/31/Justice_League_film_poster.jpg",
+                             "https://youtu.be/RWCQ22JyCL4")
+"""print(justice_league.storyline)
+wonder_woman.show_trailer()
+justice_league.show_trailer()"""
+
+movies = [wonder_woman, justice_league]
+fresh_tomatoes.open_movies_page(movies)
